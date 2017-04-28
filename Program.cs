0720e733cf8058e2050ce79c471536f15cb9952d
Geng Li <ligeng0420@gmail.com>
2017-04-28 20:00:26 +0100

    Adding channel sync and select example

--- Program.cs
+++ Program.cs
@@ -16,10 +16,52 @@ namespace ChannelExamples
             var message = channel.Receive();
             Console.WriteLine(message);
         }
+        
+        // This replicates example from: https://gobyexample.com/channel-synchronization
+        static void ChannelSychronization() 
+        {
+            var channel = new Channel<bool>();
+            Task.Run(async () => {
+                Console.Write("Working...");
+                await Task.Delay(1000);
+                Console.WriteLine("done");
+                await channel.SendAsync(true);
+            });
+            channel.ReceiveAsync().Wait();
+        }
+
+        // This replicates example from: https://gobyexample.com/select
+        static void Select() 
+        {
+            var channel1 = new Channel<String>();
+            var channel2 = new Channel<String>();
+
+            Task.Run(async () => {
+                await Task.Delay(1000);
+                await channel1.SendAsync("one");
+            });
+            Task.Run(async () => {
+                await Task.Delay(2000);
+                await channel1.SendAsync("two");
+            });
+
+            for (var i = 0; i < 2; i++) 
+            {
+                new Select()
+                    .OnReceive(channel1, msg1 => {
+                        Console.WriteLine("received " + msg1);
+                    })
+                    .OnReceive(channel2, msg2 => {
+                        Console.WriteLine("received " + msg2);
+                    }).ExecuteAsync().Wait();
+            }
+        }
 
         static void Main(string[] args)
         {
             SimpleMessage();
+            ChannelSychronization();
+            Select();
         }
     }
 }
